8d8b74d6ab5b5ede2af75b83812c655aa6087897
ivantxo <ivan.ramirezf@gmail.com>
2020-08-24 06:06:24 +1000

    Managing scream url, when closing it goes back to scream url

--- src/App.js
+++ src/App.js
@@ -68,6 +68,11 @@ class App extends Component {
                   path="/users/:handle"
                   component={user}
                 />
+                <Route
+                  exact
+                  path="/users/:handle/scream/:screamId"
+                  component={user}
+                />
               </Switch>
             </div>
           </Router>
